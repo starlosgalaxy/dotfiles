de0cb41b163f6673000669d90b3a577834464c4c
Carlos Salazar <carlosgsalazarf@gmail.com>
2025-07-28 11:27:09 -0400

    Add SSH identity files to config and create mac-setup.sh for identity management and Homebrew installation

---
+++ mac-setup.sh
@@ -0,0 +1,24 @@
+# in ~/
+
+
+# install identities (if identity exists then add to apple keychain)
+if [ -f ~/.ssh/starlosgalaxy ]; then
+    ssh-add --apple-use-keychain ~/.ssh/starlosgalaxy
+fi
+
+if [ -f ~/.ssh/carlos.salazar@fastgrid ]; then
+    ssh-add --apple-use-keychain ~/.ssh/carlos.salazar@fastgrid
+fi
+
+if [ -f ~/.ssh/carlos.salazar@slalom ]; then
+    ssh-add --apple-use-keychain ~/.ssh/carlos.salazar@slalom
+fi
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install GNU Stow
+brew install stow
+
+# install zinit
+# todo
