2ee9e4a2e30354eab52644801f76911c59afd607
Carlos Salazar <carlosgsalazarf@gmail.com>
2025-07-30 09:08:42 -0400

    Add aliases for common commands in Zsh configuration, including OS-specific adjustments for 'ls' command and confirmation prompts for file operations.

--- zsh/.config/aliases.zsh
+++ zsh/.config/aliases.zsh
@@ -1,3 +1,32 @@
 # inside ~/zsh/.config/aliases.zsh
 #!/bin/sh
-alias ls='ls --color'
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# confirm before overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+
+# Other OS specific aliases
+case "$(uname -s)" in
+
+Darwin)
+	# echo 'Mac OS X'
+	alias ls='ls -G'
+	;;
+
+Linux)
+	alias ls='ls --color=auto'
+	;;
+
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	# echo 'MS Windows'
+	;;
+*)
+	# echo 'Other OS'
+	;;
+esac
