12ce28dcdefab3fc8b3a0951d3f28631a9c7ad45
Carlos Salazar <carlosgsalazarf@gmail.com>
2025-08-06 10:04:02 -0500

    Moving Fonts to seperate folder copy fonts to useful location on system update mach setup script

--- mac.setup.sh
+++ mac.setup.sh
@@ -24,6 +24,10 @@ brew install fzf
 # install zinit
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
-# get you a nerd font (patch your own)
-# brew install fontforge
-# https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FontPatcher.zip
+# install fonts
+mkdir -p ~/Library/Fonts/Operator-Mono && find ./fonts/Operator-Mono -maxdepth 1 -name "*.otf" -print0 | xargs -0 -I {} cp {} ~/Library/Fonts/Operator-Mono/
+
+mkdir -p ~/Library/Fonts/Operator-Mono-Nerd-Font && find ./fonts/Operator-Mono-Nerd-Font -maxdepth 1 -name "*.otf" -print0 | xargs -0 -I {} cp {} ~/Library/Fonts/Operator-Mono-Nerd-Font/
+
+# some useful mac tools
+brew install --cask notunes # stops apple music from launching when play is pressed
